c3cd77038c63090b2673bf7839f2a4e06b930441
Ben <benhirsch24@gmail.com>
2013-07-02 11:19:07 -0700

    trying to force reinstalls, and use cabal 1.16.0.2

--- precompile-binaries.sh
+++ precompile-binaries.sh
@@ -49,8 +49,8 @@ sed -i "s/ld-options:/ld-options:\ -L\/app\/usr\/lib/" base.package.conf
 ghc-pkg update base.package.conf
 
 # cabal-install
-curl --silent http://hackage.haskell.org/packages/archive/cabal-install/1.16.0.1/cabal-install-1.16.0.1.tar.gz|tar xz
-cd cabal-install-1.16.0.1/
+curl --silent http://hackage.haskell.org/packages/archive/cabal-install/1.16.0.2/cabal-install-1.16.0.2.tar.gz|tar xz
+cd cabal-install-1.16.0.2/
 sh bootstrap.sh
 cd ..
 
